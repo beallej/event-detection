75fad131f43b3accd134ece44119717f292f4b62
Toberumono <JoshuaLipstone@gmail.com>
2015-10-20 15:36:02 -0500

    Added a main method for the Downloader.

---
+++ src/eventdetection/downloader/DownloaderController.java
@@ -0,0 +1,18 @@
+package eventdetection.downloader;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class DownloaderController {
+
+	public static void main(String[] args) throws IOException {
+		Downloader.loadSource(Paths.get("../Sources"));
+		try (DownloaderCollection dc = new DownloaderCollection()) {
+			FeedManager fm = new FeedManager(Paths.get("../Feeds"), Paths.get("../Scrapers"));
+			dc.addDownloader(fm);
+			for (RawArticle ra : dc.get()) {
+				System.out.println(ra.toString());
+			}
+		}
+	}
+}
