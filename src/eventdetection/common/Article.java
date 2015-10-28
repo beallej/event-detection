192e5a9b3c8240d40e9e55124dbd839869210169
ARaina <rainaa@carleton.edu>
2015-10-27 21:49:05 -0500

    Added raw and tagged text to Article.  Began fixing NLPFunction.

--- src/eventdetection/common/Article.java
+++ src/eventdetection/common/Article.java
@@ -2,18 +2,18 @@ package eventdetection.common;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 import eventdetection.downloader.Scraper;
 
 public class Article {
-	private final String title, text;
+	private final String title, taggedText, rawText;
 	private final URL url;
 	private final Source source;
-	private List<String> keywords;
-	private Map<String, String> tags;
-	
+	// private final List<String> keywords;
+	private final Map<String, String> tags;
+
 	/**
 	 * Initializes a {@link Article}
 	 * 
@@ -28,10 +28,10 @@ public class Article {
 	 * @throws MalformedURLException
 	 *             if the given <tt>url</tt> is incorrectly formatted
 	 */
-	public Article(String title, String text, String url, Source source, List<String> keywords) throws MalformedURLException {
-		this(title, text, new URL(url), source, keywords);
+	public Article(String title, String taggedText, String rawText, String url, Source source) throws MalformedURLException {
+		this(title, taggedText, rawText, new URL(url), source);
 	}
-	
+
 	/**
 	 * Initializes a {@link Article}
 	 * 
@@ -44,55 +44,55 @@ public class Article {
 	 * @param source
 	 *            the {@link Source} that the article is from
 	 */
-	public Article(String title, String text, URL url, Source source, List<String> keywords) {
+	public Article(String title, String taggedText, String rawText, URL url, Source source) {
 		this.title = title;
-		this.text = text;
+		this.taggedText = taggedText;
+		this.rawText = rawText;
 		this.url = url;
 		this.source = source;
-		this.keywords = keywords;
+		this.tags = new HashMap<>();
 	}
-	
+
 	/**
 	 * @return the title
 	 */
 	public final String getTitle() {
 		return title;
 	}
-	
+
+	/**
+	 * @return the PoS tagged text
+	 */
+	public final String getTaggedText() {
+		return taggedText;
+	}
+
 	/**
 	 * @return the scraped text
 	 */
-	public final String getText() {
-		return text;
+	public final String getRawText() {
+		return rawText;
 	}
-	
+
 	/**
 	 * @return the {@link URL} of the full article
 	 */
 	public final URL getUrl() {
 		return url;
 	}
-	
+
 	/**
 	 * @return the {@link Source}
 	 */
 	public final Source getSource() {
 		return source;
 	}
-	
-	/**
-	 * @return the keywords associated with the article
-	 */
-	
-	public final List<String> getKeywords() {
-		return keywords;
-	}
-	
+
 	@Override
 	public String toString() {
 		String out = "Title: " + getTitle();
 		out += "\nURL: " + getUrl();
-		out += "\n" + getText();
+		out += "\n" + getTaggedText();
 		return out;
 	}
 }
--- src/eventdetection/downloader/NLPFunction.java
+++ src/eventdetection/downloader/NLPFunction.java
@@ -1,80 +1,61 @@
 package eventdetection.downloader;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
 import java.util.function.Function;
 
-import com.sun.javafx.collections.MappingChange.Map;
-
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 import eventdetection.common.Article;
 
-
 public class NLPFunction implements Function<RawArticle, Article> {
+	private static final String delimiter = "_";
 
 	@Override
 	public Article apply(RawArticle t) {
-		
-		
-		Article article = null;
-	
-		
-		// creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-	    Properties props = new Properties();
-	    props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-	    
-	    // read some text in the text variable
-	    String text = t.toString();
-	    
-	    // create an empty Annotation just with the given text
-	    Annotation document = new Annotation(text);
-	    
-	    // run all Annotators on this text
-	    pipeline.annotate(document);
-	    
-	    // these are all the sentences in this document
-	    // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-	    
-	    for(CoreMap sentence: sentences) {
-	      // traversing the words in the current sentence
-	      // a CoreLabel is a CoreMap with additional token-specific methods
-	      for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-	    	  
-	        // this is the text of the token
-	        String pos = token.get(PartOfSpeechAnnotation.class);    
-	        article.tags.put(token.toString(), pos.toString());
-	        System.out.println("WORD: " + token.toString() + "     POS: " + pos.toString());
-	      }
+		// creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+		// NER, parsing, and coreference resolution
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
-	      // this is the parse tree of the current sentence
-	      //Tree tree = sentence.get(TreeAnnotation.class);
-	      
-	     // System.out.println(tree.toString());
-	    }
+		// read some text in the text variable
+		String text = t.toString();
 
-	    
+		// create an empty Annotation just with the given text
+		Annotation document = new Annotation(text);
+
+		// run all Annotators on this text
+		pipeline.annotate(document);
+
+		// these are all the sentences in this document
+		// a CoreMap is essentially a Map that uses class objects as keys and
+		// has values with custom types
+		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 		
-		
-		
-		
-		
-		return article;
-	
+		StringBuilder sb = new StringBuilder((int) (t.getText().length() * 1.5));
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
+			for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+				sb.append(token.word()).append(delimiter).append(token.get(PartOfSpeechAnnotation.class)).append(" ");
+				// this is the text of the token
+				//String pos = token.get(PartOfSpeechAnnotation.class);
+				//System.out.println("WORD: " + token.toString() + "     POS: " + pos.toString());
+			}
+
+			// this is the parse tree of the current sentence
+			// Tree tree = sentence.get(TreeAnnotation.class);
+
+			// System.out.println(tree.toString());
+		}
+		System.out.println(sb.toString());
+		return new Article(t.getTitle(), sb.toString().trim(), t.getText(), t.getUrl(), t.getSource());
 
 	}
 }
