563fb32772d544d60aaec0018e4839ecc65821a5
Toberumono <JoshuaLipstone@gmail.com>
2015-10-28 20:46:03 -0500

    Polished off the database setup files.
    Probably.

---
+++ seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO sources values ('CNN', 1.0);
+INSERT INTO feeds (id, source, url, scrapers) values ('CNN_US', 'CNN', 'http://rss.cnn.com/rss/cnn_us.rss', '["CNN"]');
--- setup.sql
+++ setup.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS eventdetection;
-USE eventdetection;
+CREATE DATABASE IF NOT EXISTS event_detection;
+USE event_detection;
 
 CREATE TABLE IF NOT EXISTS sources (
 	id varchar(255), reliability double,
@@ -7,7 +7,14 @@ CREATE TABLE IF NOT EXISTS sources (
 );
 
 CREATE TABLE IF NOT EXISTS feeds (
-	id varchar(255), source varchar(255), url blob, scrapers blob, lastseen blob,
+	id varchar(255), source varchar(255), url blob, scrapers blob, lastseen blob default NULL,
 	primary key (id),
 	foreign key (source) references sources(id)
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+	id INT UNSIGNED AUTO_INCREMENT, title varchar(255), source varchar(255), url blob, filename blob default NULL,
+	primary key (id),
+	foreign key (source) references sources(id),
+	unique key (filename(100))
 );
