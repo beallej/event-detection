a2c4f7a977f13cf11638ebfd057aaa197c9dfdb9
Phuong Dinh <phuongkhdinh@users.noreply.github.com>
2015-11-04 17:47:26 -0600

    Add SQL query

--- event_detection_web.py
+++ event_detection_web.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
+import psycopg2
+import sys
+
 app = Flask(__name__)
 
+# Establish connction with database
+try:
+    conn = psycopg2.connect(user="root", database="event_detection")
+    cursor = conn.cursor()
+except:
+    print("ERROR: Cannot establish connection with SQL database")
+    sys.exit()
+    
 @app.route("/", methods=["POST", "GET"])
 def queries():
     if request.method == "POST":
@@ -9,8 +20,15 @@ def queries():
         direct_object = request.form["direct-object"]
         indirect_object = request.form["indirect-object"]
         location = request.form["location"]
+        # Put into database
+        cursor.execute("INSERT INTO queries (subject, verb, direct_obj, indirect_obj, location) \
+                            VALUES (%s, %s, %s, %s, %s);", (subject, verb, direct_obj, indirect_obj, location))
+        
     else:
-        # we'll want to pull these from the database when we can!
+        # Get lists of query from database
+        cursor.execute("SELECT subject, verb, direct_obj, indirect_obj, location FROM queries;")
+        queries = self.cursor
+        '''
         queries = [
                     {"subject": "sub",
                      "verb": "verb",
@@ -25,19 +43,28 @@ def queries():
                      "location": None,
                      "matched": True }
                   ]
+         '''
     return render_template("queries.html", queries = queries)
 
 @app.route("/query/<id>", methods=["GET"])
 def show_query(id):
     # find query by id
     # if we don't find a query with that id, 404
-    query = {"subject": "sub",
+    cursor.execute("SELECT q.subject, q.verb, q.direct_obj, q.indirect_obj, q.location, \
+                        a.title, a.source, a.url \
+                        FROM queries q \
+                        INNER JOIN query_articles qa on q.query_id = qa.query_id\
+                        INNER JOIN articles a on qa.article_id = a.id\
+                        WHERE q.query_id = %s;", (id))
+    query = cursor
+    '''query = {"subject": "sub",
              "verb": "verb",
              "direct object": "dir obj",
              "indirect object": "ind obj",
              "location": "loc",
              "matched": False,
              "matching_articles": [] }
+    '''
     return render_template("query.html", query = query)
 
 @app.errorhandler(404)
