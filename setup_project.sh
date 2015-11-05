35b9b4ce4a656dd8dd3aa50727e3628baa92a027
Toberumono <JoshuaLipstone@gmail.com>
2015-11-04 19:33:09 -0600

    Added validator-db changes to the SQL.

--- setup_project.sh
+++ setup_project.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 brew tap 'toberumono/tap'
-brew install 'toberumono/tap/utils' 'toberumono/tap/structures' 'toberumono/tap/lexer' 'toberumono/tap/json-library'
+brew install 'toberumono/tap/utils' 'toberumono/tap/structures' 'toberumono/tap/lexer' 'toberumono/tap/json-library' 'wget'
 brew install 'postgresql' '--devel'
+echo '----------------Downloading Downloader Dependencies-------------------'
+wget '--directory-prefix=../' 'http://central.maven.org/maven2/com/rometools/rome-utils/1.5.1/rome-utils-1.5.1.jar'
+wget '--directory-prefix=../' 'http://central.maven.org/maven2/com/rometools/rome/1.5.1/rome-1.5.1.jar'
+wget '--directory-prefix=../' 'http://central.maven.org/maven2/org/jdom/jdom2/2.0.6/jdom2-2.0.6.jar'
+wget '--directory-prefix=../' 'http://central.maven.org/maven2/org/slf4j/slf4j-api/1.7.12/slf4j-api-1.7.12.jar'
+wget '--directory-prefix=../' 'http://central.maven.org/maven2/org/slf4j/slf4j-simple/1.7.12/slf4j-simple-1.7.12.jar'
+wget '--directory-prefix=../' 'http://nlp.stanford.edu/software/stanford-corenlp-full-2015-04-20.zip'
+unzip '../stanford-corenlp-full-2015-04-20.zip'
 echo '------------------Setting Up PostgreSQL Database---------------------'
 sudo gem install lunchy
 initdb /usr/local/var/postgres
