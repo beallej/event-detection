9743e7bc76041daee3bd638853665e4df26414fd
Julia Kroll <krollj@carleton.edu>
2015-11-08 08:44:28 -0600

    Actually updated setup.sql

--- setup.sql
+++ setup.sql
@@ -1,18 +1,54 @@
 CREATE USER root with SUPERUSER CREATEDB CREATEROLE INHERIT;
 
 CREATE TABLE IF NOT EXISTS sources (
-	id varchar(255), reliability real,
-	PRIMARY KEY (id)
+	id serial unique not null,
+	source_name varchar(255) unique not null,
+	reliability real,
+	primary key (id),
+	check (reliability <= 1.0)
 );
 
 CREATE TABLE IF NOT EXISTS feeds (
-	id varchar(255), source varchar(255), url text, scrapers text, lastseen text default NULL,
-	primary key (id),
-	foreign key (source) references sources(id)
+	id serial unique not null,
+	feed_name varchar(255) unique not null,
+	source integer not null references sources(id) ON DELETE CASCADE,
+	url text unique not null,
+	scrapers text[] default '{}',
+	lastseen text default NULL,
+	primary key (id)
 );
 
 CREATE TABLE IF NOT EXISTS articles (
-	id serial, title varchar(255), source varchar(255), url text, filename text default NULL,
-	primary key (id),
-	foreign key (source) references sources(id)
+	id serial unique not null,
+	title varchar(255) not null,
+	source integer not null references sources(id) ON DELETE CASCADE,
+	url text unique not null,
+	filename text default NULL,
+	keywords text[] default NULL,
+	primary key (id)
+);
+
+CREATE TABLE IF NOT EXISTS queries (
+	id serial,
+	subject varchar(255),
+	verb varchar(255),
+	direct_obj varchar(255),
+	indirect_obj varchar(255),
+	loc varchar(255),
+	primary key (id)
+);
+
+CREATE TABLE IF NOT EXISTS query_words (
+	word varchar(255) not null,
+	pos varchar(255) not null,
+	sense varchar(255) default NULL,
+	synonyms varchar(255)[] default '{}',
+	primary key (word, pos, sense),
+	constraint word_pos unique(word, pos)
+);
+
+CREATE TABLE IF NOT EXISTS query_articles (
+	query integer references queries(id) ON DELETE CASCADE,
+	article integer references articles(id) ON DELETE CASCADE,
+	primary key (query, article)
 );
