a6069a1d43c0812f46fd3317fe54a06677bfa238
Toberumono <JoshuaLipstone@gmail.com>
2015-11-09 11:21:16 -0600

    Reworked how notifications are tracked in the database.

--- setup.sql
+++ setup.sql
@@ -1,54 +1,59 @@
 CREATE USER root with SUPERUSER CREATEDB CREATEROLE INHERIT;
 
-create domain contact_method_array as varchar(32)[] default '{}' constraint contact_methods_constraint check ('{"email", "phone"}'::varchar[] @> value::varchar[]);
-
 CREATE TABLE IF NOT EXISTS sources (
-	id serial unique not null,
+	id serial primary key unique not null,
 	source_name varchar(255) unique not null,
 	reliability real,
-	primary key (id),
 	check (reliability <= 1.0)
 );
 
 CREATE TABLE IF NOT EXISTS feeds (
-	id serial unique not null,
+	id serial primary key unique not null,
 	feed_name varchar(255) unique not null,
 	source integer not null references sources(id) ON DELETE CASCADE,
 	url text unique not null,
 	scrapers text[] default '{}',
-	lastseen text default null,
-	primary key (id)
+	lastseen text default null
 );
 
 CREATE TABLE IF NOT EXISTS articles (
-	id serial unique not null,
+	id serial primary key unique not null,
 	title varchar(255) not null,
 	source integer not null references sources(id) ON DELETE CASCADE,
 	url text unique not null,
 	filename text default null,
-	keywords text[] default null,
-	primary key (id)
+	keywords text[] default null
 );
 
 CREATE TABLE IF NOT EXISTS users (
-	id serial,
-	user_name text not null,
+	id serial primary key unique not null,
+	user_name text unique not null,
 	phone varchar(32) default null,
-	email varchar(255) default null,
-	primary key (id)
+	email varchar(255) default null
+);
+
+CREATE TABLE IF NOT EXISTS notification_types (
+	id serial primary key unique not null,
+	type_name text unique not null,
+	description text default ''
 );
 
 CREATE TABLE IF NOT EXISTS queries (
-	id serial,
+	id serial primary key unique not null,
 	userid integer not null references users(id) ON DELETE CASCADE,
 	subject varchar(255),
 	verb varchar(255),
 	direct_obj varchar(255),
 	indirect_obj varchar(255),
 	loc varchar(255),
-	contact_methods contact_method_array,
-	primary key (id),
 	constraint unique_queries unique (userid, subject, verb, direct_obj, indirect_obj, loc)
+);
+
+CREATE TABLE IF NOT EXISTS query_notifications (
+	query_id integer not null references queries(id) ON DELETE CASCADE,
+	notification_type integer not null references notification_types(id) ON DELETE CASCADE,
+	arguments text[] default '{}',
+	constraint unique_notification unique (query_id, notification_type, arguments)
 );
 
 CREATE TABLE IF NOT EXISTS query_words (
