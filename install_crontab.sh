09ee0598203db98c261a409c66ecdad8a6705790
Toberumono <JoshuaLipstone@gmail.com>
2015-11-11 14:34:55 -0600

    Created a crontab installer.

---
+++ install_crontab.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+working_dir="$(pwd)"
+java_path="$(which java)"
+[ "$(python --version | grep 'Python 3')" != "" ] && python_path="$(which python)" || python_path="$(which python3)"
+if [ "$python_path" == "" ]; then
+	>&2 echo "Error:  Unable to find the executable for python 3."
+	exit 1
+fi
+line="$java_path -jar $(dirname $(pwd))/event-detection.jar $(pwd)/configuration.json && $python_path $(pwd)/interface.py"
+line="0 * * * * $line"
+read -p "Preparing to write: $line. Is this okay? [y/N] " yn
+yn=$(echo "${yn:0:1}" | tr '[:upper:]' '[:lower:]')
+if [ "$yn" != "y" ]; then
+	echo "Stopping."
+	exit 1
+else
+	echo "Writing."
+fi
+unset yn
+(crontab -l; echo "$line") | crontab -
