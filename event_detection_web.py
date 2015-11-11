e290ae1a78b334d9ec9d2258e705348ae5854c51
Laura Biester <laurajpb@gmail.com>
2015-11-11 17:59:29 -0600

    Updated queries

--- event_detection_web.py
+++ event_detection_web.py
@@ -53,19 +53,15 @@ def query(id):
     # find query by id
     # if we don't find a query with that id, 404
     con, cursor = connect_database()
-    cursor.execute("SELECT q.subject, q.verb, q.direct_obj, q.indirect_obj, q.loc, \
-                        a.title, a.source, a.url \
-                        FROM queries q \
-                        INNER JOIN query_articles qa on q.id = qa.query\
-                        INNER JOIN articles a on qa.article = a.id\
-                        WHERE q.id = %s;", id)
+    cursor.execute("SELECT a.title, s.source_name as source, a.url \
+                    FROM queries q \
+                    INNER JOIN query_articles qa on q.id = qa.query \
+                    INNER JOIN articles a on qa.article = a.id \
+                    INNER JOIN sources s on s.id = a.source \
+                    WHERE q.id = %s;", (id,))
     articles = cursor.fetchall()
 
-    # mock data
-    articles = [{ "title" : "Meet the futurists: People who 'live in the future'", "source" : "CNN", "url" : "http://www.cnn.com/2015/11/06/tech/pioneers-futurists/index.html"},
-                { "title" : "'I no longer see a fat little boy,' says a man overcoming body dysmorphia", "source" : "CNN", "url" : "http://www.cnn.com/2015/11/06/health/brian-cuban-body-dysmorphia-turning-points/index.html"}]
-
-    cursor.execute("SELECT id, subject, verb, direct_obj, indirect_obj, loc FROM queries where id = %s;", id)
+    cursor.execute("SELECT id, subject, verb, direct_obj, indirect_obj, loc FROM queries where id = %s;", (id,))
     query = cursor.fetchone()
 
     if con:
