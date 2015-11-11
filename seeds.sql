9993194259027f933004eea4dd15815d37e81f09
Toberumono <JoshuaLipstone@gmail.com>
2015-11-10 21:59:56 -0600

    Fixed seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO sources (source_name, reliability) values ('CNN', 1.0);
 INSERT INTO feeds (feed_name, source, url, scrapers) values ('CNN_US', 1, 'http://rss.cnn.com/rss/cnn_us.rss', '{"CNN"}');
-INSERT INTO users (user_name, email) values ('dummy', 'event.detection.carleton@gmail.com');
+INSERT INTO users (email) values ('event.detection.carleton@gmail.com');
