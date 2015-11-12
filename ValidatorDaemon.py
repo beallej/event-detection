49a1cf50f2fbd80a685951b659eb165432356d56
Julia Kroll <krollj@carleton.edu>
2015-11-11 20:49:10 -0600

    Forgot to save files

--- ValidatorDaemon.py
+++ ValidatorDaemon.py
@@ -5,10 +5,13 @@
 from Validator import *
 import DataSource
 
-ds = DataSource()
-unprocessed_pairs = ds.get_unprocessed_query_article_pairs()
-for pair in unprocessed_pairs:
-	query_id = pair[0]
-	article_id = pair[1]
-	matching_prob = Validator.validate(query_id, article_id)
-	post_validator_update(matching_prob, query_id, article_id)
+class ValidatorDaemon():
+
+	def run():
+		ds = DataSource()
+		unprocessed_pairs = ds.get_unprocessed_query_article_pairs()
+		for pair in unprocessed_pairs:
+			query_id = pair[0]
+			article_id = pair[1]
+			matching_prob = Validator.validate(query_id, article_id)
+			post_validator_update(matching_prob, query_id, article_id)
