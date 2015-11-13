40d43a4bee0cda5103ba12b05e05c4abcfae27a9
Toberumono <JoshuaLipstone@gmail.com>
2015-11-12 20:19:19 -0600

    Added process synchronization.

--- ArticleProcessorDaemon.py
+++ ArticleProcessorDaemon.py
@@ -7,17 +7,26 @@ from KeywordExtractor import *
 import json
 from Validator import Article
 import pdb
+import fcntl
 
 class ArticleProcessorDaemon:
     def run(self):
-        ds = DataSource()
-        extractor = KeywordExtractor()
-        unprocessed_articles = ds.get_unprocessed_articles()
-        for article in unprocessed_articles:
-            body = open("articles/{0}".format(article[2])).read()
-            article_with_body = Article(article[1], body, article[3], article[4])
-            keywords = extractor.extract_keywords(article_with_body)
-            keyword_string = json.dumps(keywords)
-            ds.add_keywords_to_article(article[0], keyword_string)
+        fd, fo = 0, 0
+        try:
+            fo = open("~/.event-detection-active", "wb")
+            fd = fo.fileno()
+            fcntl.lockf(fd, fcntl.LOCK_EX)
+            ds = DataSource()
+            extractor = KeywordExtractor()
+            unprocessed_articles = ds.get_unprocessed_articles()
+            for article in unprocessed_articles:
+                body = open("articles/{0}".format(article[2])).read()
+                article_with_body = Article(article[1], body, article[3], article[4])
+                keywords = extractor.extract_keywords(article_with_body)
+                keyword_string = json.dumps(keywords)
+                ds.add_keywords_to_article(article[0], keyword_string)
+        finally:
+            fcntl.lockf(fd, fcntl.LOCK_UN)
+            fo.close()
 
 ArticleProcessorDaemon().run()
