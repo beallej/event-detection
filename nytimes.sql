935fc5803cff59b61b74d8e477863d7365d6e51f
Laura Biester <laurajpb@gmail.com>
2016-01-15 21:33:16 -0600

    NYTIMES added as source

--- nytimes.sql
+++ nytimes.sql
@@ -1,5 +1,2 @@
-INSERT INTO sources (source_name, reliability) values ('CNN', 1.0);
-INSERT INTO feeds (feed_name, source, url, scrapers) values ('CNN_US', 1, 'http://rss.cnn.com/rss/cnn_us.rss', '{"CNN"}');
 INSERT INTO SOURCES (source_name, reliability) VALUES ('NYTIMES', 1);
 INSERT INTO feeds (feed_name, source, url, scrapers) values ('NYTIMES', (SELECT id FROM sources WHERE source_name = 'NYTIMES'), 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', '{"NYTIMES"}');
-INSERT INTO users (email) values ('event.detection.carleton@gmail.com');
