aa50063246f66676d65183705418928c1304a1b5
Julia Kroll <krollj@carleton.edu>
2016-01-16 15:10:19 -0600

    Calculate K using two different methods

--- Matrix.py
+++ Matrix.py
@@ -9,16 +9,23 @@ class Matrix:
 
 	def __init__(self):
 		num_entries = 0
+		num_datapoints = 0
 
 	def get_num_entries(self):
 		return self.num_entries
 
+	def get_num_datapoints(self):
+		return self.num_datapoints
+
 	def get_article_titles(self, filename):
 		titles = []
 		f = open(filename)
+		num_datapoints = 0
 		for line in f.readlines():
 			title = line.strip('\n')
 			titles.append(title)
+			num_datapoints += 1
+		self.num_datapoints = num_datapoints
 		return titles
 
 	def get_vocabulary_set(self, titles):
@@ -43,13 +50,13 @@ class Matrix:
 		num_words = len(vocabulary_list)
 		num_titles = len(titles)
 		matrix = zeros((num_titles, num_words))
-		num_entries_count = 0
+		num_entries = 0
 		for w_idx, word in enumerate(vocabulary_list):
 			for t_idx, title in enumerate(titles):
 				if title_vocabs[title][word] > 0:
 					matrix[t_idx,w_idx] = title_vocabs[title][word]
-					num_entries_count += 1
-		self.num_entries = num_entries_count
+					num_entries += 1
+		self.num_entries = num_entries
 		return matrix
 
 	def get_matrix(self, filename):
--- ScipyKmeans.py
+++ ScipyKmeans.py
@@ -3,6 +3,7 @@
 
 from scipy.cluster.vq import kmeans, vq, whiten
 from Matrix import *
+from math import sqrt
 
 m = Matrix()
 
@@ -10,9 +11,22 @@ article_titles = m.get_article_titles('article_titles.txt')
 matrix = m.get_matrix('article_titles.txt')
 whitened_matrix = whiten(matrix)
 
+# Compute k
+# Two methods taken from https://en.wikipedia.org/wiki/Determining_the_number_of_clusters_in_a_data_set
+
+
+# Method 1: Using (m*n)/t, matrix dimensions over number of entries
+shape = whitened_matrix.shape
+num_articles = shape[0]
+num_keywords = shape[1]
+num_entries = m.get_num_entries()
+text_databases_k = (num_articles * num_keywords) // num_entries 
+
+# Method 2: Square root of (number of documents / 2)
+rule_of_thumb_k = round(sqrt(m.get_num_datapoints()))
+
 # Compute k-means with k clusters
-k = 10
-codebook, distortion = kmeans(whitened_matrix, k)
+codebook, distortion = kmeans(whitened_matrix, text_databases_k)
 
 # Assign each article title to a cluster
 cluster_ids, distortion = vq(whitened_matrix, codebook)
