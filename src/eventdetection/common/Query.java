fd7565548d77d515c83d79e8d93765785b7610d5
Toberumono <JoshuaLipstone@gmail.com>
2016-01-17 21:06:07 -0600

    Commented Query.

--- src/eventdetection/common/Query.java
+++ src/eventdetection/common/Query.java
@@ -3,11 +3,24 @@ package eventdetection.common;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Represents a query for the validator.
+ * 
+ * @author Joshua Lipstone
+ */
 public class Query {
 	private final int id;
 	private final String subject, verb, directObject, indirectObject, location;
 	private final boolean processed;
 	
+	/**
+	 * Initializes a {@link Query} using the values in a {@link ResultSet}
+	 * 
+	 * @param rs
+	 *            the {@link ResultSet} to use
+	 * @throws SQLException
+	 *             if there is an error reading the {@link ResultSet}
+	 */
 	public Query(ResultSet rs) throws SQLException {
 		id = rs.getInt("id");
 		subject = rs.getString("subject");
@@ -18,6 +31,24 @@ public class Query {
 		processed = rs.getBoolean("processed");
 	}
 	
+	/**
+	 * Initializes a {@link Query} with the specific field values
+	 * 
+	 * @param id
+	 *            the ID as it is in the database
+	 * @param subject
+	 *            the subject
+	 * @param verb
+	 *            the verb
+	 * @param directObject
+	 *            the direct object
+	 * @param indirectObject
+	 *            the indirect object
+	 * @param location
+	 *            the location
+	 * @param processed
+	 *            whether the {@link Query} has been preprocessed
+	 */
 	public Query(int id, String subject, String verb, String directObject, String indirectObject, String location, Boolean processed) {
 		this.id = id;
 		this.subject = subject;
