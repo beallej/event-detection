2cce21aaceb763542ce37195ce96b50dd75ba4be
Josie Bealle <beallej@carleton.edu>
2016-01-25 21:42:25 -0600

    better validation

--- AbstractClusterer.py
+++ AbstractClusterer.py
@@ -1,6 +1,6 @@
 from MatrixCreator import *
 from math import sqrt
-
+from ElbowMethodK import *
 class AbstractClusterer:
     """
     Abstact Clusterer: An interface for clusters, which cluster articles
@@ -34,6 +34,8 @@ class AbstractClusterer:
         else:
             cutoff = self.get_average_k()
         self.k = cutoff
+        # self.k = get_best_k(matrix)
+        # print(self.k, "MEOW")
 
         return matrix
 
--- ClusterValidator.py
+++ ClusterValidator.py
@@ -11,7 +11,7 @@ class ClusterValidator:
     class ClusterValidator: Uses keywords from an article to validate a query
     """
 
-    MIN_THRESHOLD = 0
+    MIN_THRESHOLD = 0.1
 
     def __init__(self, cluster_class, k_function):
         """
@@ -36,34 +36,44 @@ class ClusterValidator:
         :param query: query to get the best cluster for
         :return: a cluster or None (if below a certain threshold)
         """
-        max_match_value = 0
-        match_value = 0
+
         # Need to process query and article formats
         ds = DataSource()
         query_synonyms_raw = ds.get_query_synonyms(query_id)
         query_synonyms = {}
-        for w in query_synonyms_raw:
-            if w[1] not in query_synonyms:
-                query_synonyms[w[1]] = {}
-            query_synonyms[w[1]][w[0]] = w[3]
 
+        for w in query_synonyms_raw:
+            query_synonyms[w[0]] = w[3]
+        print(query_synonyms)
         cluster_matches = {}
+
         for cluster in self.clusters:
+            #print(cluster.article_titles)
+
             max_match_value = 0
+            match_value = 0
             cluster_keywords = cluster.get_keywords()
-            for pos in query_synonyms:
-                for query_word in query_synonyms[pos]:
-                    max_match_value += 2
-                    if pos in cluster_keywords:
-                        cluster_keywords_with_same_tag = cluster_keywords[pos][0]
-                        #Compare main key. If match, match_value += 2
-                        if query_word in cluster_keywords_with_same_tag:
-                            match_value += 2
-                        else:
-                            for synonym in query_synonyms[pos][query_word]:
-                                if synonym in cluster_keywords_with_same_tag:
-                                    match_value += 1
-                                    break
+            all_cluster_keywords = set()
+
+            # flatten cluster_keywords
+            for pos in cluster_keywords:
+                all_cluster_keywords.update(cluster_keywords[pos])
+            print(all_cluster_keywords)
+            # find matches
+            for query_word in query_synonyms:
+                max_match_value += 2
+                if query_word in all_cluster_keywords:
+                    match_value += 2
+                    print(query_word)
+                else:
+                    synonyms_matched = 0
+                    for synonym in query_synonyms[query_word]:
+                        if synonym in all_cluster_keywords:
+                            match_value += 1
+                            synonyms_matched += 1
+                            print(synonym)
+                            break
+                    #match_value += synonyms_matched/len(query_synonyms[query_word])
             match_percentage = match_value / max_match_value
             cluster_matches[cluster] = match_percentage
 
@@ -75,8 +85,8 @@ class ClusterValidator:
         return None
 
 def main():
-    clusterValidator = ClusterValidator(KMeansClusterer, "get_average_k")
-    result = clusterValidator.validate(3)
+    clusterValidator = ClusterValidator(HierarchicalAgglomerativeClusterer, None)
+    result = clusterValidator.validate(7)
     if result is None:
         print("No clusters found")
     else:
