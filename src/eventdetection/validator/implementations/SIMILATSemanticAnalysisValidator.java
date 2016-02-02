f0de5d5bd07d4b84c746caeaf9de55b6853c4a86
Phuong Dinh <dinhp@carleton.edu>
2016-02-01 23:19:01 -0600

    basic post validation-Phuong,Anmol & Julia

--- src/eventdetection/validator/implementations/SIMILATSemanticAnalysisValidator.java
+++ src/eventdetection/validator/implementations/SIMILATSemanticAnalysisValidator.java
@@ -15,6 +15,7 @@ import semilar.sentencemetrics.PairwiseComparer.WordWeightType;
 import semilar.tools.preprocessing.SentencePreprocessor;
 import semilar.tools.semantic.WordNetSimilarity;
 import semilar.wordmetrics.LSAWordMetric;
+import semilar.wordmetrics.LDAWordMetric;
 import semilar.wordmetrics.WNWordMetric;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,12 +26,17 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.text.*;
+import edu.sussex.nlp.jws.*;
+import java.util.Properties;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
 
 import toberumono.structures.collections.lists.SortedList;
 import toberumono.structures.tuples.Pair;
 import toberumono.structures.SortingMethods;
 
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.*;
+import edu.stanford.nlp.ling.*;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.util.CoreMap;
 import eventdetection.common.Article;
@@ -68,6 +74,8 @@ import java.util.concurrent.Future;
 import toberumono.json.JSONArray;
 import toberumono.json.JSONObject;
 import toberumono.json.JSONSystem;
+import eventdetection.downloader.POSTagger;
+
 
 /**
  * A SEMILAR validator using prebuilt library from http://deeptutor2.memphis.edu/
@@ -111,6 +119,7 @@ public class SIMILATSemanticAnalysisValidator extends Validator {
     //BLEU (introduced for machine translation evaluation):http://acl.ldc.upenn.edu/P/P02/P02-1040.pdf 
     BLEUComparer bleuComparer;
     LSAComparer lsaComparer;
+    WNWordMetric wnMetricLin;
     LexicalOverlapComparer lexicalOverlapComparer; // Just see the lexical overlap.
     //For LDA based method.. see the separate example file. Its something different.
     
@@ -132,11 +141,11 @@ public class SIMILATSemanticAnalysisValidator extends Validator {
 //    public articleSentenceSentenceSimilarityTest() {
 
         /* Word to word similarity expanded to sentence to sentence .. so we need word metrics */
-//        boolean wnFirstSenseOnly = false; //applies for WN based methods only.
-//        WNWordMetric wnMetricLin = new WNWordMetric(WordNetSimilarity.WNSimMeasure.LIN, wnFirstSenseOnly);
-//        WNWordMetric wnMetricLeskTanim = new WNWordMetric(WordNetSimilarity.WNSimMeasure.LESK_TANIM, wnFirstSenseOnly);
+        boolean wnFirstSenseOnly = false; //applies for WN based methods only.
+        wnMetricLin = new WNWordMetric(WordNetSimilarity.WNSimMeasure.LIN, wnFirstSenseOnly);
+        //WNWordMetric wnMetricLeskTanim = new WNWordMetric(WordNetSimilarity.WNSimMeasure.LESK_TANIM, wnFirstSenseOnly);
         //provide the LSA model name you want to use.
-        LSAWordMetric lsaMetricTasa = new LSAWordMetric("LSA-MODEL-TASA-LEMMATIZED-DIM300");
+        //LSAWordMetric lsaMetricTasa = new LSAWordMetric("LSA-MODEL-TASA-LEMMATIZED-DIM300");
         //provide the LDA model name you want to use.
         //LDAWordMetric ldaMetricTasa = new LDAWordMetric("LDA-MODEL-TASA-LEMMATIZED-TOPIC300");
 
@@ -145,26 +154,26 @@ public class SIMILATSemanticAnalysisValidator extends Validator {
         //greedyComparerLSATasa = new GreedyComparer(lsaMetricTasa, 0.3f, false);
         //greedyComparerLDATasa = new GreedyComparer(ldaMetricTasa, 0.3f, false);
 
-        //optimumComparerWNLin = new OptimumComparer(wnMetricLin, 0.3f, false, WordWeightType.NONE, NormalizeType.AVERAGE);
+        optimumComparerWNLin = new OptimumComparer(wnMetricLin, 0.3f, false, WordWeightType.NONE, NormalizeType.AVERAGE);
         //optimumComparerWNLeskTanim = new OptimumComparer(wnMetricLeskTanim, 0.3f, false, WordWeightType.NONE, NormalizeType.AVERAGE);
-//        optimumComparerLSATasa = new OptimumComparer(lsaMetricTasa, 0.3f, false, WordWeightType.NONE, NormalizeType.AVERAGE);
+        //optimumComparerLSATasa = new OptimumComparer(lsaMetricTasa, 0.3f, false, WordWeightType.NONE, NormalizeType.AVERAGE);
         //optimumComparerLDATasa = new OptimumComparer(ldaMetricTasa, 0.3f, false, WordWeightType.NONE, NormalizeType.AVERAGE);
 
         //Use one of the many word metrics. The example below uses Wordnet Lesk Tanim. Similarly, try using other
         //word similarity metrics.
-        //dependencyComparerWnLeskTanim = new DependencyComparer(wnMetricLeskTanim, 0.3f, true, "NONE", "AVERAGE");
+        //dependencyComparerWnLeskTanim = new DependencyComparer(wnMetricLin, 0.3f, true, "NONE", "AVERAGE");
 
         /* methods without using word metrics */
-        cmComparer = new CorleyMihalceaComparer(0.3f, false, "NONE", "par");
+        //cmComparer = new CorleyMihalceaComparer(0.3f, false, "NONE", "par");
         //for METEOR, please provide the **Absolute** path to your project home folder (without / at the end), And the
         // semilar library jar file should be in your project home folder.
         //meteorComparer = new MeteorComparer("C:/Users/Rajendra/workspace/SemilarLib/");
-//        bleuComparer = new BLEUComparer();
+        //bleuComparer = new BLEUComparer();
 
         //lsaComparer: This is different from lsaMetricTasa, as this method will
         // directly calculate sentence level similarity whereas  lsaMetricTasa
         // is a word 2 word similarity metric used with Optimum and Greedy methods.
-        lsaComparer = new LSAComparer("LSA-MODEL-TASA-LEMMATIZED-DIM300");
+        //lsaComparer = new LSAComparer("LSA-MODEL-TASA-LEMMATIZED-DIM300");
         //lexicalOverlapComparer = new LexicalOverlapComparer(false);  // use base form of words? - No/false. 
         //for LDA based method.. please see the different example file.
     }
@@ -180,7 +189,7 @@ public class SIMILATSemanticAnalysisValidator extends Validator {
         
         SentencePreprocessor preprocessor = new SentencePreprocessor(SentencePreprocessor.TokenizerType.STANFORD, SentencePreprocessor.TaggerType.STANFORD, SentencePreprocessor.StemmerType.PORTER, SentencePreprocessor.ParserType.STANFORD);
         
-        SortedList<Pair<Double, String>> topN = new SortedList<>((a, b) -> b.getX().compareTo(a.getX()));
+        SortedList<Pair<Double, CoreMap>> topN = new SortedList<>((a, b) -> b.getX().compareTo(a.getX()));
         
         StringBuilder phrase1 = new StringBuilder();
 		phrase1.append(query.getSubject()).append(" ").append(query.getVerb());
@@ -196,31 +205,103 @@ public class SIMILATSemanticAnalysisValidator extends Validator {
         
         Double temp;
 
-        
+        String title = article.getAnnotatedTitle().toString();
+        Sentence articleTitle = preprocessor.preprocessSentence(title);
+        Double tempTitle = (double) optimumComparerWNLin.computeSimilarity(querySentence, articleTitle);
+
         for (Annotation paragraph : article.getAnnotatedText()) {
 			List<CoreMap> sentences = paragraph.get(SentencesAnnotation.class);
+
 			for (CoreMap sentence : sentences) {
                 String sen = POSUtils.reconstructSentence(sentence);
                 articleSentence = preprocessor.preprocessSentence(sen);
-                temp = (double) lsaComparer.computeSimilarity(querySentence, articleSentence);
+                temp = (double) optimumComparerWNLin.computeSimilarity(querySentence, articleSentence);
                 if (temp.equals(Double.NaN))
                     continue;
-                topN.add(new Pair<>(temp, sen));
+                topN.add(new Pair<>(temp, sentence));
                 if (topN.size() > MAX_SENTENCES)
                     topN.remove(topN.size() - 1);
             }
         }
         
-  
+        //If article > threshold
+        //CHeck if top 5 sentences are great
+        //If not reduce the probibility to 0
+
   		double average = 0.0;
-		for (Pair<Double, String> p : topN){
+		for (Pair<Double, CoreMap> p : topN){
 			average += p.getX();
-            System.out.println(p.getY() + p.getX());
+            System.out.println(p.getY().toString() + p.getX());
         }
-		average /= (double) topN.size(); 
-        //System.out.println("AVERGARE OF SEMILAR = " + average);
-        return new ValidationResult[]{new ValidationResult(this.getID(), article.getID(), average)};
+        average /= (double) topN.size(); 
+        double validation = 0.0;
+        if (average > 0.15) {
+            validation = postProcess(topN, query,phrase1.toString());
+        }
+        System.out.println("Annotated title: "+ article.getAnnotatedTitle());
+        System.out.println("ARTICLE  ID " + article.getID() + " average: "+average + " title: "+tempTitle);
+        return new ValidationResult[]{new ValidationResult(this.getID(), article.getID(), validation)};
     }
     
-   
+    public double postProcess(SortedList<Pair<Double, CoreMap>> topN, Query query, String rawQuery){
+        String subject, dirObject, indirObject;
+        subject = query.getSubject();
+        dirObject = "";
+        if (query.getDirectObject() != null && query.getDirectObject().length() > 0)
+            dirObject = query.getDirectObject();
+        if (query.getIndirectObject() != null && query.getIndirectObject().length() > 0)
+            indirObject = query.getIndirectObject();
+        // if (query.getLocation() != null && query.getLocation().length() > 0)
+        //     phrase1.append(" ").append(query.getLocation()); 
+        // if (query.getLocation() != null && query.getLocation().length() > 0)
+        //     query.getLocation();  
+        ArrayList<String> keywordNouns = new ArrayList<String>();
+        //Annotation taggedQuery = POSTagger.annotate(rawQuery);
+        // Annotation taggedQuery = POSTagger.tag(annotatedQuery);
+        StanfordCoreNLP pipeline;
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, depparse");
+        pipeline = System.getProperty("enable.pos", "true").toLowerCase().charAt(0) == 't' ? new StanfordCoreNLP(props) : null;
+        Annotation taggedQuery = new Annotation(rawQuery);
+        pipeline.annotate(taggedQuery);
+
+        for (CoreLabel token: taggedQuery.get(TokensAnnotation.class)){
+                String pos = token.get(PartOfSpeechAnnotation.class);
+                if (pos.length() > 1 && pos.substring(0,2).equals("NN")){
+                    if (subject.contains(token.get(LemmaAnnotation.class))){
+                        keywordNouns.add(token.get(LemmaAnnotation.class));
+                    }
+                    if (dirObject.contains(token.get(LemmaAnnotation.class))){
+                        keywordNouns.add(token.get(LemmaAnnotation.class));
+                    }
+                    //Can add ind Obj later
+            }
+            }
+
+        int matchedSentenceOutOf5 = 0;
+        for (Pair<Double, CoreMap> p : topN){ //for each sentence
+            int matchedPerSentence = 0;
+            for (CoreLabel token: p.getY().get(TokensAnnotation.class)){ //each word in sentence
+                String pos = token.get(PartOfSpeechAnnotation.class);
+                String lemma = token.get(LemmaAnnotation.class);
+                if (pos.length() > 1 && pos.substring(0,2).equals("NN")){
+                    //System.out.println(article.getID() +"POS tag for word : " + token + " tag: " + pos + " lemma " + lemma);
+                    for (String imptNoun:keywordNouns){
+                        double matched = wnMetricLin.computeWordSimilarityNoPos(lemma, imptNoun);
+                        if (matched>0.65){
+                            matchedPerSentence += 1;
+                        }
+                    }
+                }
+            }
+            if (matchedPerSentence > 0){
+                matchedSentenceOutOf5 += 1;
+            }
+        }
+        if (matchedSentenceOutOf5 > 1){
+            return 1.0;
+        } 
+        return 0.0;
+
+    }   
 }
