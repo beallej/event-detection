aed53b7e1892ad258726b03331c032713d5000ef
Josie Bealle <beallej@carleton.edu>
2016-02-09 01:14:49 -0600

    testing code

--- Tester.py
+++ Tester.py
@@ -2,6 +2,9 @@ from DataSource import *
 import numpy as np
 from scipy import mean
 import scikits.bootstrap as bootstrap
+import matplotlib.pyplot as plot
+import matplotlib.patches as mpatches
+from psycopg2.extras import RealDictCursor
 
 class Tester:
     def __init__(self):
@@ -12,15 +15,22 @@ class Tester:
         self.results = self.dataSource.get_validation_results()
         self.article_ids = self.dataSource.get_articles()
         self.query_ids = self.dataSource.get_queries()
-        self.algorithm_ids = self.dataSource.get_algorithms()
+        self.algorithms = self.dataSource.get_algorithms()
 
         self.best_thresholds = {}
 
-        self.bootstrap()
-        for algorithm_id in self.article_ids:
+        #self.bootstrap()
+        X = np.arange(0,1,0.025)
+        labels = []
+        Y_vals = []
+        for algorithm in self.algorithms:
+            algorithm_id = algorithm[0]
+            algorithm_name = algorithm[1]
+            labels.append(algorithm_name)
             best_threshold = 0
             best_f1 = 0
-            for threshold in np.arange(0,1,0.1):
+            Y = []
+            for threshold in X:
                 f1_measures = []
                 for article_left_out in self.article_ids:
                     f1_measures.append(self.f1(article_left_out, algorithm_id, threshold))
@@ -28,19 +38,35 @@ class Tester:
                 if avg_f1 > best_f1:
                     best_f1 = avg_f1
                     best_threshold = threshold
+                Y.append(avg_f1)
+            Y_vals.append(Y)
+
             self.best_thresholds[algorithm_id] = (best_f1, best_threshold)
+        self.plot_threshold_and_results(X, labels, Y_vals)
 
 
 
-        #leave one out
-        #threshold
-        #precision,recall  - bootstrapping and forreal
+    def plot_threshold_and_results(self, x, labels, y_vals):
+        colors = ["red", "blue", "yellow", "green", "orange", "purple", "pink"]
+        color_index = 0
+        key_legends = []
+        for y_i, y in enumerate(y_vals):
+            plot.scatter(x, y, color=colors[color_index])
+            legend = mpatches.Patch(color=colors[color_index], label = labels[y_i])
+            key_legends.append(legend)
+            color_index = (color_index + 1)% len(colors)
+        plot.legend(handles=key_legends)
+        plot.title('F1 Measure with different thresholds for different algorithms')
+        plot.xlabel("Threshold")
+        plot.ylabel("F1 Measure")
+        plot.show()
+
+
 
     #TODO: should we separate out the algorithms???
     def precision_bootstrap(self, dataset):
         true_positives = 0
         false_positives = 0
-        false_negatives = 0
         for datum in dataset:
             query_id = datum[0]
             article_id = datum[1]
@@ -54,16 +80,12 @@ class Tester:
                 true_positives += 1
             elif test_value and not actual_value:
                 false_positives += 1
-            elif not test_value and actual_value:
-                false_negatives += 1
 
-        precision = true_positives/(true_positives + false_positives)
-        return precision
+        return self.precision(true_positives, false_positives)
 
         #TODO: should we separate out the algorithms???
     def recall_bootstrap(self, dataset):
         true_positives = 0
-        false_positives = 0
         false_negatives = 0
         for datum in dataset:
             query_id = datum[0]
@@ -76,19 +98,18 @@ class Tester:
             test_value = (test_value_probability > 0.0)
             if test_value and actual_value:
                 true_positives += 1
-            elif test_value and not actual_value:
-                false_positives += 1
             elif not test_value and actual_value:
                 false_negatives += 1
-
-        recall = true_positives/(true_positives + false_negatives)
-        return recall
+        return self.recall(true_positives, false_negatives)
 
 
     def bootstrap(self):
 
-        CIs = bootstrap.ci(data=list(self.results), statfunction=self.precision_bootstrap)
-        print("Bootstrapped 95% confidence intervals\nLow:", CIs[0], "\nHigh:", CIs[1])
+        # CIs = bootstrap.ci(data=list(self.results), statfunction=self.precision_bootstrap)
+        # print("Bootstrapped 95% confidence intervals for precision \nLow:", CIs[0], "\nHigh:", CIs[1])
+
+        CIs = bootstrap.ci(data=list(self.results), statfunction=self.recall_bootstrap, n_samples=1)
+        print("Bootstrapped 95% confidence intervals for recall \nLow:", CIs[0], "\nHigh:", CIs[1])
 
 
     #I know this is inneficient we can fix it later
@@ -109,30 +130,57 @@ class Tester:
                     elif not test_value and actual_value:
                         false_negatives += 1
 
-        recall = true_positives/(true_positives + false_negatives)
-        precision = true_positives/(true_positives + false_positives)
+        recall = self.recall(true_positives, false_negatives)
+        precision = self.precision(true_positives, false_positives)
+
+        #TODO: FIX THIS
+        if recall + precision == 0:
+            return 0
         f1 = 2 * (precision * recall)/(precision + recall)
 
         return f1
 
+    def precision(self, true_positives, false_positives):
+        if true_positives + false_positives == 0:
+            return 1
+        return true_positives/(true_positives + false_positives)
+
+    def recall(self, true_positives, false_negatives):
+        if true_positives + false_negatives == 0:
+            return 1
+        return true_positives/(true_positives + false_negatives)
 
 
 
 
-
-class TesterDataSource(DataSource):
+class TesterDataSource:
     def __init__(self):
-        super(TesterDataSource, self).__init__()
+        db = Globals.database
+        try:
+            conn = psycopg2.connect(user='root', database=db)
+            conn.autocommit = True
+        except psycopg2.Error:
+            print("Error: cannot connect to event_detection database")
+            sys.exit()
+        try:
+            self.cursor = conn.cursor(cursor_factory=RealDictCursor)
+        except psycopg2.Error:
+            print("Error: cannot create cursor")
+            sys.exit()
+
         self.query_articles = None
         self.validation_results = None
+
 
     def get_algorithms(self):
         """
         gets a list of algorithm ids
         :return: the list
         """
-        self.cursor.execute("SELECT id FROM validation_algorithms")
-        return self.cursor.fetchall()
+        # self.cursor.execute("SELECT id FROM validation_algorithms")
+        # return self.cursor.fetchall()
+
+        return[(2, "Swoogle Semantic Analysis"), (4, "TextRank Swoogle Semantic Analysis")]
 
     def get_queries(self):
         """
@@ -140,7 +188,8 @@ class TesterDataSource(DataSource):
         :return: the list
         """
         self.cursor.execute("SELECT id FROM queries")
-        return self.cursor.fetchall()
+        queries = self.cursor.fetchall()
+        return [query["id"] for query in queries]
 
     def get_query_articles(self):
         """
@@ -151,8 +200,26 @@ class TesterDataSource(DataSource):
         if self.query_articles is None:
             self.cursor.execute("SELECT query, article, validates FROM query_articles")
             query_articles_list = self.cursor.fetchall()
-            self.query_articles = {(qa[0], qa[1]) : qa[2] for qa in query_articles_list}
+            self.query_articles = {(qa["query"], qa["article"]) : qa["validates"] for qa in query_articles_list}
         return self.query_articles
+
+    # def get_query_articles(self):
+    #     """
+    #     Gets a list of all query_articles and if the query validates the article
+    #     Caches the list
+    #     :return: the list of query articles
+    #     """
+    #     if self.query_articles is None:
+    #         self.cursor.execute("SELECT query, article, validates FROM query_articles")
+    #         self.query_articles = self.cursor.fetchall()
+    #     return self.query_articles()
+
+
+    def get_articles(self):
+        self.cursor.execute("SELECT id FROM articles")
+        articles = self.cursor.fetchall()
+        return [article["id"] for article in articles]
+
 
     def get_validation_result(self, query_id, article_id, algorithm_id):
         """
