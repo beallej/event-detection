7580181f255ab5ae1a205fbcf94295655da301f9
Toberumono <JoshuaLipstone@gmail.com>
2016-02-08 18:24:19 -0600

    Greatly improved SubprocessHelpers.

--- src/eventdetection/common/SubprocessHelpers.java
+++ src/eventdetection/common/SubprocessHelpers.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 /**
  * Provides useful methods for working with subprocesses.
@@ -66,15 +67,30 @@ public class SubprocessHelpers {
 	 *             if an error occurs while starting the process
 	 */
 	public static Process executePythonProcess(Path scriptPath, String... args) throws IOException {
-		String[] command = {bashPath, "-l", "-c", ""};
-		StringBuilder cmd = new StringBuilder(pythonPath.length() * 2);
-		cmd.append(pythonPath).append(" ").append(scriptPath.normalize().toString());
-		for (String arg : args)
-			cmd.append(" ").append(arg);
-		command[3] = cmd.toString();
+		return executePythonProcess(scriptPath, scriptPath.getParent(), args);
+	}
+	
+	/**
+	 * Executes a python script with the given arguments
+	 * 
+	 * @param scriptPath
+	 *            the {@link Path} to the script.
+	 * @param directory
+	 *            a {@link Path} to the directory from which to run the script
+	 * @param args
+	 *            the arguments to the script. Arguments that include spaces must be quoted
+	 * @return a started {@link Process}
+	 * @throws IOException
+	 *             if an error occurs while starting the process
+	 */
+	public static Process executePythonProcess(Path scriptPath, Path directory, String... args) throws IOException {
+		String[] command = {bashPath, "-c", pythonPath.toString() + " \"$@\"", "-", scriptPath.normalize().toString()};
+		command = Arrays.copyOf(command, command.length + args.length);
+		for (int i = 0; i < args.length; i++)
+			command[i + 5] = args[i];
 		
 		ProcessBuilder pb = new ProcessBuilder(command);
-		pb.directory(scriptPath.getParent().toFile());
+		pb.directory(directory.toFile());
 		return pb.start();
 	}
 }
