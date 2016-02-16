33c8c5fa3649cb2d32d099296103b11be7e5ea7c
Toberumono <JoshuaLipstone@gmail.com>
2016-02-15 22:43:29 -0600

    Shuffled a few things in seeds in order to avoid conflicts with existing databases.

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,7 @@
 INSERT INTO sources (source_name, reliability) values ('CNN', 1.0), ('REUTERS', 1.0), ('ABC', 1.0);
-INSERT INTO feeds (feed_name, source, url, scrapers) values ('CNN_US', (SELECT id FROM sources WHERE source_name = 'CNN'), 'http://rss.cnn.com/rss/cnn_us.rss', '{"CNN"}'),
-	('REUTERS_TOP_NEWS', (SELECT id FROM sources WHERE source_name = 'REUTERS'), 'http://feeds.reuters.com/reuters/topNews', '{"REUTERS"}'), ('ABC_TOP_STORIES', (SELECT id FROM sources WHERE source_name = 'ABC'), 'http://feeds.abcnews.com/abcnews/topsto    ries', '{"ABC"}');
+INSERT INTO feeds (feed_name, source, url, scrapers) values ('CNN_US', (SELECT id FROM sources WHERE source_name = 'CNN'), 'http://rss.cnn.com/rss/cnn_us.rss', '{"CNN"}');
+INSERT INTO feeds (feed_name, source, url, scrapers) values ('REUTERS_TOP_NEWS', (SELECT id FROM sources WHERE source_name = 'REUTERS'), 'http://feeds.reuters.com/reuters/topNews', '{"REUTERS"}');
+INSERT INTO feeds (feed_name, source, url, scrapers) values ('ABC_TOP_STORIES', (SELECT id FROM sources WHERE source_name = 'ABC'), 'http://feeds.abcnews.com/abcnews/topstories', '{"ABC"}');
 INSERT INTO users (email) values ('event.detection.carleton@gmail.com');
 INSERT INTO validation_algorithms (algorithm, base_class, validator_type, parameters) values
 	('Keyword', 'eventdetection.validator.implementations.KeywordValidator', 'OneToOne', '"KeywordValidator.json"');
