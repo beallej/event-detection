00438bd16d0294e2afb6222e4ab5a0f60361759b
Laura Biester <laurajpb@gmail.com>
2016-02-15 21:55:15 -0600

    Added ABC news scraper

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO sources (source_name, reliability) values ('CNN', 1.0), ('REUTERS', 1.0);
+INSERT INTO sources (source_name, reliability) values ('CNN', 1.0), ('REUTERS', 1.0), ('ABC', 1.0);
 INSERT INTO feeds (feed_name, source, url, scrapers) values ('CNN_US', (SELECT id FROM sources WHERE source_name = 'CNN'), 'http://rss.cnn.com/rss/cnn_us.rss', '{"CNN"}'),
-	('REUTERS_TOP_NEWS', (SELECT id FROM sources WHERE source_name = 'REUTERS'), 'http://feeds.reuters.com/reuters/topNews', '{"REUTERS"}');
+	('REUTERS_TOP_NEWS', (SELECT id FROM sources WHERE source_name = 'REUTERS'), 'http://feeds.reuters.com/reuters/topNews', '{"REUTERS"}'), ('ABC_TOP_STORIES', (SELECT id FROM sources WHERE source_name = 'ABC'), 'http://feeds.abcnews.com/abcnews/topsto    ries', '{"ABC"}');
 INSERT INTO users (email) values ('event.detection.carleton@gmail.com');
 INSERT INTO validation_algorithms (algorithm, base_class, validator_type, parameters) values
 	('Keyword', 'eventdetection.validator.implementations.KeywordValidator', 'OneToOne', '"KeywordValidator.json"');
