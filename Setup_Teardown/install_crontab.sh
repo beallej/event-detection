6a755e0d639c593b677f56fd6b0c8c4439debd29
Toberumono <JoshuaLipstone@gmail.com>
2016-02-19 23:49:19 -0600

    Fixed install_crontab.sh

--- Setup_Teardown/install_crontab.sh
+++ Setup_Teardown/install_crontab.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-working_dir="$(pwd)"
+working_dir="$(pwd)/"
 java_path="$(which java)"
-[ "$(python --version | grep 'Python 3')" != "" ] && python_path="$(which python)" || python_path="$(which python3)"
-if [ "$python_path" == "" ]; then
-	>&2 echo "Error:  Unable to find the executable for python 3."
-	exit 1
-fi
-line="$java_path -jar $(pwd)/event-detection.jar $(pwd)/configuration.json && $python_path $(pwd)/Daemons/ArticleProcessorDaemon.py && $python_path $(pwd)/Daemons/ValidatorDaemon.py"
+line="$java_path -jar ${working_dir}pipeline.jar ${working_dir}configuration.json"
 line="0 * * * * $line"
 read -p "Preparing to write: $line. Is this okay? [y/N] " yn
 yn=$(echo "${yn:0:1}" | tr '[:upper:]' '[:lower:]')
