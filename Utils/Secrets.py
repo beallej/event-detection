013cdec92cf38ace29105c70d944840c7e3e67c0
Josie Bealle <beallej@carleton.edu>
2016-02-29 17:36:44 -0600

    secret keys are not for everybody

---
+++ Utils/Secrets.py
@@ -0,0 +1,11 @@
+import sys; import os
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+
+from_email = ""
+twilio_number = ""
+twilio_account_sid = ""
+twilio_auth_token = ""
+sendgrid_api_key = ""
+bitly_api_login = ""
+bitly_api_key = ""
